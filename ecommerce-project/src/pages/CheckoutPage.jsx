2481e9609b9e96cc5cdd744f89b1cc4470173530
pratiush pathak <pratiushpathak@gmail.com>
2025-09-16 01:32:07 +0530

    move checkout page into react

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,7 +1,12 @@
+import './checkout-header.css'
+import './CheckoutPage.css'
+
 export function CheckoutPage()
 {
   return(
     <>
+       <title>Checkout</title>
+ 
   <div className="checkout-header">
       <div className="header-content">
         <div className="checkout-header-left-section">
--- ecommerce-project/src/pages/homePage.jsx
+++ ecommerce-project/src/pages/homePage.jsx
@@ -5,6 +5,7 @@
 {
   return(
     <>
+       <title>Ecommerce Project</title>
        <div className="header">
       <div className="left-section">
         <a href="index.html" className="header-link">
